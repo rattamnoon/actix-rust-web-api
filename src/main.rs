cbfebebe184caa67702f49b554aba1d85e45b421
UN <59855096+rattamnoon@users.noreply.github.com>
2024-03-25 10:12:55 +0700

    mysql
    Signed-off-by: UN <59855096+rattamnoon@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,92 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use mysql_async::prelude::*;
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct Payment {
+    customer_id: i32,
+    amount: i32,
+    account_name: Option<String>,
+}
+
+#[tokio::main]
+async fn create_table_and_query() -> Result<(), mysql_async::Error> {
+    let payments = vec![
+        Payment {
+            customer_id: 1,
+            amount: 2,
+            account_name: None,
+        },
+        Payment {
+            customer_id: 3,
+            amount: 4,
+            account_name: Some("foo".into()),
+        },
+        Payment {
+            customer_id: 5,
+            amount: 6,
+            account_name: None,
+        },
+        Payment {
+            customer_id: 7,
+            amount: 8,
+            account_name: None,
+        },
+        Payment {
+            customer_id: 9,
+            amount: 10,
+            account_name: Some("bar".into()),
+        },
+    ];
+
+    let database_url = "mysql://root:password@localhost:3306/rust";
+
+    let pool = mysql_async::Pool::new(database_url);
+    let mut conn = pool.get_conn().await?;
+
+    // Create a temporary table
+    r"CREATE TEMPORARY TABLE payment (
+        customer_id int not null,
+        amount int not null,
+        account_name text
+    )"
+    .ignore(&mut conn)
+    .await?;
+
+    // Save payments
+    r"INSERT INTO payment (customer_id, amount, account_name)
+      VALUES (:customer_id, :amount, :account_name)"
+        .with(payments.iter().map(|payment| {
+            params! {
+                "customer_id" => payment.customer_id,
+                "amount" => payment.amount,
+                "account_name" => payment.account_name.as_ref(),
+            }
+        }))
+        .batch(&mut conn)
+        .await?;
+
+    // Load payments from the database. Type inference will work here.
+    let loaded_payments = "SELECT customer_id, amount, account_name FROM payment"
+        .with(())
+        .map(&mut conn, |(customer_id, amount, account_name)| Payment {
+            customer_id,
+            amount,
+            account_name,
+        })
+        .await?;
+
+    // Dropped connection will go to the pool
+    drop(conn);
+
+    // The Pool must be disconnected explicitly because
+    // it's an asynchronous operation.
+    pool.disconnect().await?;
+
+    assert_eq!(loaded_payments, payments);
+
+    // the async fn returns Result, so
+    Ok(())
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -14,12 +102,21 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+#[get("/create_table_and_query")]
+async fn create_table_and_query_handler() -> impl Responder {
+    match create_table_and_query() {
+        Ok(_) => HttpResponse::Ok().body("create_table_and_query success"),
+        Err(e) => HttpResponse::Ok().body(format!("create_table_and_query failed: {}", e)),
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
+            .service(create_table_and_query_handler)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
